f65e8de187d926544e91b5916ba3efac3d747e18
NikolausHouben <nikolaus.houben@gmail.com>
2023-08-08 01:42:30 -0700

    moved customed metrics to train_eval

--- bin/train_eval.py
+++ bin/train_eval.py
@@ -5,6 +5,9 @@ import wandb
 import pandas as pd
 import numpy as np
 import time
+from typing import Callable, Optional, Sequence, Tuple, Union
+
+
 
 import plotly.express as px
 import plotly.graph_objects as go
@@ -20,7 +23,7 @@ from darts.dataprocessing.transformers.boxcox import BoxCox
 from darts.dataprocessing.transformers.scaler import Scaler
 from darts.dataprocessing.transformers.missing_values_filler import MissingValuesFiller
 from darts.dataprocessing import Pipeline
-from darts.metrics import rmse, r2_score, mae, smape, mape, max_peak_error , mean_n_peak_error
+from darts.metrics import rmse, r2_score, mae, smape, mape
 from darts.models import (
                             BlockRNNModel, NBEATSModel, RandomForest, 
                             LightGBMModel, XGBModel, LinearRegressionModel, TFTModel, TransformerModel
@@ -528,6 +531,347 @@ def predict_testset(model, ts, ts_covs, n_lags, n_ahead, eval_stride, pipeline):
     ts_predictions_inverse = pipeline.inverse_transform(ts_predictions) # inverse transform the predictions, we need the original values for the evaluation
     
     return ts_predictions_inverse.pd_series().to_frame('prediction'), score
+
+
+from darts.logging import get_logger, raise_if_not, raise_log
+from darts.utils import _build_tqdm_iterator, _parallel_apply
+from functools import wraps
+from inspect import signature
+logger = get_logger(__name__)
+
+def multi_ts_support(func):
+    """
+    This decorator further adapts the metrics that took as input two univariate/multivariate ``TimeSeries`` instances,
+    adding support for equally-sized sequences of ``TimeSeries`` instances. The decorator computes the pairwise metric
+    for ``TimeSeries`` with the same indices, and returns a float value that is computed as a function of all the
+    pairwise metrics using a `inter_reduction` subroutine passed as argument to the metric function.
+
+    If a 'Sequence[TimeSeries]' is passed as input, this decorator provides also parallelisation of the metric
+    evaluation regarding different ``TimeSeries`` (if the `n_jobs` parameter is not set 1).
+    """
+
+    @wraps(func)
+    def wrapper_multi_ts_support(*args, **kwargs):
+        actual_series = (
+            kwargs["actual_series"] if "actual_series" in kwargs else args[0]
+        )
+        pred_series = (
+            kwargs["pred_series"]
+            if "pred_series" in kwargs
+            else args[0]
+            if "actual_series" in kwargs
+            else args[1]
+        )
+
+        n_jobs = kwargs.pop("n_jobs", signature(func).parameters["n_jobs"].default)
+        verbose = kwargs.pop("verbose", signature(func).parameters["verbose"].default)
+
+        raise_if_not(isinstance(n_jobs, int), "n_jobs must be an integer")
+        raise_if_not(isinstance(verbose, bool), "verbose must be a bool")
+
+        actual_series = (
+            [actual_series]
+            if not isinstance(actual_series, Sequence)
+            else actual_series
+        )
+        pred_series = (
+            [pred_series] if not isinstance(pred_series, Sequence) else pred_series
+        )
+
+        raise_if_not(
+            len(actual_series) == len(pred_series),
+            "The two TimeSeries sequences must have the same length.",
+            logger,
+        )
+
+        num_series_in_args = int("actual_series" not in kwargs) + int(
+            "pred_series" not in kwargs
+        )
+        kwargs.pop("actual_series", 0)
+        kwargs.pop("pred_series", 0)
+
+        iterator = _build_tqdm_iterator(
+            iterable=zip(actual_series, pred_series),
+            verbose=verbose,
+            total=len(actual_series),
+        )
+
+        value_list = _parallel_apply(
+            iterator=iterator,
+            fn=func,
+            n_jobs=n_jobs,
+            fn_args=args[num_series_in_args:],
+            fn_kwargs=kwargs,
+        )
+
+        # in case the reduction is not reducing the metrics sequence to a single value, e.g., if returning the
+        # np.ndarray of values with the identity function, we must handle the single TS case, where we should
+        # return a single value instead of a np.array of len 1
+
+        if len(value_list) == 1:
+            value_list = value_list[0]
+
+        if "inter_reduction" in kwargs:
+            return kwargs["inter_reduction"](value_list)
+        else:
+            return signature(func).parameters["inter_reduction"].default(value_list)
+
+    return wrapper_multi_ts_support
+
+
+def multivariate_support(func):
+    """
+    This decorator transforms a metric function that takes as input two univariate TimeSeries instances
+    into a function that takes two equally-sized multivariate TimeSeries instances, computes the pairwise univariate
+    metrics for components with the same indices, and returns a float value that is computed as a function of all the
+    univariate metrics using a `reduction` subroutine passed as argument to the metric function.
+    """
+
+    @wraps(func)
+    def wrapper_multivariate_support(*args, **kwargs):
+        # we can avoid checks about args and kwargs since the input is adjusted by the previous decorator
+        actual_series = args[0]
+        pred_series = args[1]
+
+        raise_if_not(
+            actual_series.width == pred_series.width,
+            "The two TimeSeries instances must have the same width.",
+            logger,
+        )
+
+        value_list = []
+        for i in range(actual_series.width):
+            value_list.append(
+                func(
+                    actual_series.univariate_component(i),
+                    pred_series.univariate_component(i),
+                    *args[2:],
+                    **kwargs
+                )
+            )  # [2:] since we already know the first two arguments are the series
+        if "reduction" in kwargs:
+            return kwargs["reduction"](value_list)
+        else:
+            return signature(func).parameters["reduction"].default(value_list)
+
+    return wrapper_multivariate_support
+
+
+def _get_values(
+    series: TimeSeries, stochastic_quantile: Optional[float] = 0.5
+) -> np.ndarray:
+    """
+    Returns the numpy values of a time series.
+    For stochastic series, return either all sample values with (stochastic_quantile=None) or the quantile sample value
+    with (stochastic_quantile {>=0,<=1})
+    """
+    if series.is_deterministic:
+        series_values = series.univariate_values()
+    else:  # stochastic
+        if stochastic_quantile is None:
+            series_values = series.all_values(copy=False)
+        else:
+            series_values = series.quantile_timeseries(
+                quantile=stochastic_quantile
+            ).univariate_values()
+    return series_values
+
+
+def _get_values_or_raise(
+    series_a: TimeSeries,
+    series_b: TimeSeries,
+    intersect: bool,
+    stochastic_quantile: Optional[float] = 0.5,
+    remove_nan_union: bool = False,
+) -> Tuple[np.ndarray, np.ndarray]:
+    """Returns the processed numpy values of two time series. Processing can be customized with arguments
+    `intersect, stochastic_quantile, remove_nan_union`.
+
+    Raises a ValueError if the two time series (or their intersection) do not have the same time index.
+
+    Parameters
+    ----------
+    series_a
+        A univariate deterministic ``TimeSeries`` instance (the actual series).
+    series_b
+        A univariate (deterministic or stochastic) ``TimeSeries`` instance (the predicted series).
+    intersect
+        A boolean for whether or not to only consider the time intersection between `series_a` and `series_b`
+    stochastic_quantile
+        Optionally, for stochastic predicted series, return either all sample values with (`stochastic_quantile=None`)
+        or any deterministic quantile sample values by setting `stochastic_quantile=quantile` {>=0,<=1}.
+    remove_nan_union
+        By setting `remove_non_union` to True, remove all indices from `series_a` and `series_b` which have a NaN value
+        in either of the two input series.
+    """
+
+    raise_if_not(
+        series_a.width == series_b.width,
+        "The two time series must have the same number of components",
+        logger,
+    )
+
+    raise_if_not(isinstance(intersect, bool), "The intersect parameter must be a bool")
+
+    series_a_common = series_a.slice_intersect(series_b) if intersect else series_a
+    series_b_common = series_b.slice_intersect(series_a) if intersect else series_b
+
+    raise_if_not(
+        series_a_common.has_same_time_as(series_b_common),
+        "The two time series (or their intersection) "
+        "must have the same time index."
+        "\nFirst series: {}\nSecond series: {}".format(
+            series_a.time_index, series_b.time_index
+        ),
+        logger,
+    )
+
+    series_a_det = _get_values(series_a_common, stochastic_quantile=stochastic_quantile)
+    series_b_det = _get_values(series_b_common, stochastic_quantile=stochastic_quantile)
+
+    if not remove_nan_union:
+        return series_a_det, series_b_det
+
+    b_is_deterministic = bool(len(series_b_det.shape) == 1)
+    if b_is_deterministic:
+        isnan_mask = np.logical_or(np.isnan(series_a_det), np.isnan(series_b_det))
+    else:
+        isnan_mask = np.logical_or(
+            np.isnan(series_a_det), np.isnan(series_b_det).any(axis=2).flatten()
+        )
+    return np.delete(series_a_det, isnan_mask), np.delete(
+        series_b_det, isnan_mask, axis=0
+    )
+
+
+
+@multi_ts_support
+@multivariate_support
+def max_peak_error(
+    actual_series: Union[TimeSeries, Sequence[TimeSeries]],
+    pred_series: Union[TimeSeries, Sequence[TimeSeries]],
+    intersect: bool = True,
+    *,
+    reduction: Callable[[np.ndarray], float] = np.mean,
+    inter_reduction: Callable[[np.ndarray], Union[float, np.ndarray]] = lambda x: x,
+    n_jobs: int = 1,
+    verbose: bool = False
+) -> Union[float, np.ndarray]:
+    """Mean Absolute Error (MAE).
+
+    For two time series :math:`y^1` and :math:`y^2` of length :math:`T`, it is computed as
+
+    .. math:: \\frac{1}{T}\\sum_{t=1}^T{(|y^1_t - y^2_t|)}.
+
+    If any of the series is stochastic (containing several samples), the median sample value is considered.
+
+    Parameters
+    ----------
+    actual_series
+        The (sequence of) actual series.
+    pred_series
+        The (sequence of) predicted series.
+    intersect
+        For time series that are overlapping in time without having the same time index, setting `True`
+        will consider the values only over their common time interval (intersection in time).
+    reduction
+        Function taking as input a ``np.ndarray`` and returning a scalar value. This function is used to aggregate
+        the metrics of different components in case of multivariate ``TimeSeries`` instances.
+    inter_reduction
+        Function taking as input a ``np.ndarray`` and returning either a scalar value or a ``np.ndarray``.
+        This function can be used to aggregate the metrics of different series in case the metric is evaluated on a
+        ``Sequence[TimeSeries]``. Defaults to the identity function, which returns the pairwise metrics for each pair
+        of ``TimeSeries`` received in input. Example: ``inter_reduction=np.mean``, will return the average of the
+        pairwise metrics.
+    n_jobs
+        The number of jobs to run in parallel. Parallel jobs are created only when a ``Sequence[TimeSeries]`` is
+        passed as input, parallelising operations regarding different ``TimeSeries``. Defaults to `1`
+        (sequential). Setting the parameter to `-1` means using all the available processors.
+    verbose
+        Optionally, whether to print operations progress
+
+    Returns
+    -------
+    float
+        The maximum peak error
+    """
+
+    y1, y2 = _get_values_or_raise(
+        actual_series, pred_series, intersect, remove_nan_union=True
+    )
+    
+    y1_max = np.max(y1)
+    y1_idx_max = np.argmax(y1)
+    y2_max = y2[y1_idx_max]
+
+    return np.abs(y1_max - y2_max)
+    
+
+@multi_ts_support
+@multivariate_support
+def mean_n_peak_error(
+    actual_series: Union[TimeSeries, Sequence[TimeSeries]],
+    pred_series: Union[TimeSeries, Sequence[TimeSeries]],
+    intersect: bool = True,
+    *,
+    reduction: Callable[[np.ndarray], float] = np.mean,
+    inter_reduction: Callable[[np.ndarray], Union[float, np.ndarray]] = lambda x: x,
+    n_jobs: int = 1,
+    verbose: bool = False,
+    n: int = 5
+) -> Union[float, np.ndarray]:
+    """Mean N Peak Error.
+
+    For two time series :math:`y^1` and :math:`y^2` of length :math:`T`, it is computed as
+
+    .. math:: 
+
+    If any of the series is stochastic (containing several samples), the median sample value is considered.
+
+    Parameters
+    ----------
+    actual_series
+        The (sequence of) actual series.
+    pred_series
+        The (sequence of) predicted series.
+    intersect
+        For time series that are overlapping in time without having the same time index, setting `True`
+        will consider the values only over their common time interval (intersection in time).
+    reduction
+        Function taking as input a ``np.ndarray`` and returning a scalar value. This function is used to aggregate
+        the metrics of different components in case of multivariate ``TimeSeries`` instances.
+    inter_reduction
+        Function taking as input a ``np.ndarray`` and returning either a scalar value or a ``np.ndarray``.
+        This function can be used to aggregate the metrics of different series in case the metric is evaluated on a
+        ``Sequence[TimeSeries]``. Defaults to the identity function, which returns the pairwise metrics for each pair
+        of ``TimeSeries`` received in input. Example: ``inter_reduction=np.mean``, will return the average of the
+        pairwise metrics.
+    n_jobs
+        The number of jobs to run in parallel. Parallel jobs are created only when a ``Sequence[TimeSeries]`` is
+        passed as input, parallelising operations regarding different ``TimeSeries``. Defaults to `1`
+        (sequential). Setting the parameter to `-1` means using all the available processors.
+    verbose
+        Optionally, whether to print operations progress
+
+    Returns
+    -------
+    float
+        The mean peak error for the top n peaks
+    """
+
+    y1, y2 = _get_values_or_raise(
+        actual_series, pred_series, intersect, remove_nan_union=True
+    )
+    
+    y1_sorted_indices = np.argsort(y1)  # Get indices that would sort array y1 in ascending order
+    y1_top5_indices = y1_sorted_indices[-n:]  # Get the indices of the top 5 highest values in y1
+    
+    y1_top5_max = np.max(y1[y1_top5_indices])  # Get the maximum value among the top 5 values in y1
+    y2_top5_max = np.max(y2[y1_top5_indices])  # Get the corresponding value in y2
+    mean_difference = np.mean(np.abs(y1_top5_max - y2_top5_max))
+
+    return mean_difference
+
 
 
 
